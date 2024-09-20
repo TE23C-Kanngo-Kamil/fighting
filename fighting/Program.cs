82be6492e9a1825e6a004ef938bbb142376870a1
Kamil Kanngo <kamil.kanngo@elev.ga.ntig.se>
2024-09-20 13:58:04 +0200

    first

--- fighting/Program.cs
+++ fighting/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Skriv ditt namn:");
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hej {name}!");
+if (name == "")
+{
+    Console.WriteLine("Skriv ditt namn för i helvete!")
+    name = Console.ReadLine();
+}
+
+Console.ReadLine();
