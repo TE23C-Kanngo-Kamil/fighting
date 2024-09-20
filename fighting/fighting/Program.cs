dfd8196ff1c38a0257cf608c5021d6281297a659
Kamil Kanngo <kamil.kanngo@elev.ga.ntig.se>
2024-09-20 14:53:29 +0200

    sjej

--- fighting/fighting/Program.cs
+++ fighting/fighting/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Write your name:");
+string name = Console.ReadLine();
+
+while (name.Length < 3)
+{
+    Console.WriteLine("Write your name, damn it!");
+    name = Console.ReadLine();
+}
+
+Console.WriteLine($"Hello {name}!");
+Console.ReadLine();
+
+int heroHp = 100;
+int villainHp = 100;
+
+string heroName = "HERO";
+string villainName = "VILLAIN";
+
+Random generator = new Random();
+
+while (heroHp > 0 && villainHp > 0)
+{
+    Console.WriteLine("---- ===== NEW ROUND ==== ----");
+    Console.WriteLine($"{heroName}: {heroHp} {villainName}: {villainHp}");
+
+    int heroDamage = generator.Next(21);
+    villainHp -= heroDamage;
+    villainHp = Math.Max(0, villainHp);
+    Console.WriteLine($"{heroName} dealt {heroDamage} damage to {villainName}");
+
+    int villainDamage = generator.Next(21);
+    heroHp -= villainDamage;
+    heroHp = Math.Max(0, heroHp);
+    Console.WriteLine($"{villainName} dealt {villainDamage} damage to {heroName}");
+}
