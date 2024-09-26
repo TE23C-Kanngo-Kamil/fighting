934c7e297502cbe2563f863140b3993eb5b97eb0
Kamil Kanngo <kamil.kanngo@elev.ga.ntig.se>
2024-09-26 12:33:06 +0200

    Färdig med slagsmålsspelet!

--- fighting/fighting/Program.cs
+++ fighting/fighting/Program.cs
@@ -13,8 +13,8 @@ Console.ReadLine();
 int heroHp = 100;
 int villainHp = 100;
 
-string heroName = "HERO";
-string villainName = "VILLAIN";
+string heroName = "Drake";
+string villainName = "Kendrick";
 
 Random generator = new Random();
 
@@ -24,12 +24,41 @@ while (heroHp > 0 && villainHp > 0)
     Console.WriteLine($"{heroName}: {heroHp} {villainName}: {villainHp}");
 
     int heroDamage = generator.Next(21);
+    if (heroDamage == 0)
+    {
+          Console.WriteLine($"A miss!");
+    }
     villainHp -= heroDamage;
     villainHp = Math.Max(0, villainHp);
     Console.WriteLine($"{heroName} dealt {heroDamage} damage to {villainName}");
 
     int villainDamage = generator.Next(21);
+    if (villainDamage == 0)
+    {
+        Console.WriteLine($"A miss!");
+    }
     heroHp -= villainDamage;
     heroHp = Math.Max(0, heroHp);
     Console.WriteLine($"{villainName} dealt {villainDamage} damage to {heroName}");
+
+    Console.WriteLine("Press any key to continue");
+    Console.ReadKey();
 }
+
+Console.WriteLine("---- ===== BATTLE OVER ==== ----");
+
+if (heroHp == 0 && villainHp == 0)
+{
+    Console.WriteLine("It is a draw!");
+}
+else if (heroHp == 0)
+{
+    Console.WriteLine($"{villainName} is the winner!");
+}
+else
+{
+    Console.WriteLine($"{heroName} is the winner!");
+}
+
+Console.WriteLine($"Thank you for playing, {name}! Press any key to exit");
+Console.ReadKey();
